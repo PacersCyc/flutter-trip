eda54756e562d63125806fc6034dbeae18f6056d
PacersCyc <452720855@qq.com>
2020-03-02 13:46:01 +0800

    android 插件编写

--- android/app/src/main/java/com/example/flutter_trip/MainActivity.java
+++ android/app/src/main/java/com/example/flutter_trip/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.flutter_trip;
 
 import androidx.annotation.NonNull;
+
+import com.example.plugin.asr.AsrPlugin;
+
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
